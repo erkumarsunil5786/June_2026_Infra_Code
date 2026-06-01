c9d9e5d150fcb7afa1fb5f328d3cda1c1063d899
erkumarsunil <erkumarsunil5786@gmail.com>
2026-06-01 22:29:26 +0530

    rg & stg code add

---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "satya-rg"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "stg" {
+    depends_on = [ azurerm_resource_group.rg ]
+  name                     = "satya2026stgacc"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+}
